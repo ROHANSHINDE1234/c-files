e721b529cedfc247a80dea8e33bd30df13067906
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-08-15 17:54:26 +0530

    Add files via upload

---
+++ codechef/beginner/population.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int final(int a, int b, int c)
+{
+    int population;
+    population = a - b + c;
+    printf("%d", population);
+    printf("\n");
+}
+int main(void)
+{
+    int T;
+    printf("Enter the number of test cases: ");
+    scanf("%d", &T);
+    int a[T][3];
+    for (int i = 0; i < T; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for (int i = 0; i < T; i++)
+    {
+        final(a[i][0], a[i][1], a[i][2]);
+    }
+
+    return 0;
+}
