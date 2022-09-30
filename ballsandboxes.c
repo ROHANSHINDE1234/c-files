1718162c8306fd1e794b9dff12877ef8430357ce
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-09-30 22:34:30 +0530

    Update ballsandboxes.c

--- ballsandboxes.c
+++ ballsandboxes.c
@@ -1,40 +1,26 @@
 #include <stdio.h>
 int main()
 {
-    int T;
-    printf("Enter the number of Test cases: \n");
-    scanf("%d", &T);
-    while (T--)
+    int t;
+    scanf("%d", &t);
+    while (t--)
     {
-        int N, K;
-        printf("Enter the number of Balls and number of Boxes: \n");
-        scanf("%d %d", &N, &K);
-        if (N < K)
-        {
-            printf("No\n");
-        }
-        else if (N == K)
-        {
-            if (N == 1)
-            {
-                printf("Yes\n");
-            }
-            else
-            {
-                printf("No\n");
-            }
-        }
-        else
-        {
-            if (N>(2*K))
-            {
-                printf("Yes\n");
-            }
-            else
-            {
-                printf("No\n");
-            }
-        }   
+        long k, n, sum = 0;
+        scanf("%ld%ld", &n, &k);
+
+        sum = ((k + 1) * k) / 2;
+
+        if (n == 1 && k == 1)
+            printf("yes\n");
+        else if (n == k && n == 1)
+            printf("no\n");
+        else if (k > n)
+            printf("no\n");
+
+        else if (n >= sum)
+            printf("yes\n");
+        else if (n < sum)
+            printf("no\n");
     }
     return 0;
 }
