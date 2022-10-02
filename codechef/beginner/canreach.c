937ce2bc88c5f4c6ba0d6e1600f0ab78a9d83795
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-10-02 23:45:27 +0530

    Add files via upload

---
+++ codechef/beginner/canreach.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int T;
+    printf("Enter the number of Test cases: \n");
+    scanf("%d", &T);
+    while (T--)
+    {
+        int x,y,K,a,b;
+        printf("Enter the number cordinates in x,y formaat and enter the step size: \n");
+        scanf("%d %d %d",&x,&y,&K);
+        a=((-1)*(x));
+        b=((-1)*(y));
+        if (x!=a)
+        {
+            /* code */
+        }
+        
+    }
+    return 0;
+}
