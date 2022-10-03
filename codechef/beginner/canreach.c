f29c13a22c69cc2822dfc279a43798ea148e661b
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-10-03 21:43:33 +0530

    Update canreach.c

--- codechef/beginner/canreach.c
+++ codechef/beginner/canreach.c
@@ -6,16 +6,32 @@ int main()
     scanf("%d", &T);
     while (T--)
     {
-        int x,y,K,a,b;
+        int x, y, K, a, b;
+        int count = 0;
         printf("Enter the number cordinates in x,y formaat and enter the step size: \n");
-        scanf("%d %d %d",&x,&y,&K);
-        a=((-1)*(x));
-        b=((-1)*(y));
-        if (x!=a)
+        scanf("%d %d %d", &x, &y, &K);
+        if (x < 0)
+            x = -1 * x;
+
+        if (y < 0)
+            y = -1 * y;
+
+        for (int i = 1; i <= x; i++)
         {
-            /* code */
+            a = i * K;
+            if (a == x)
+                ++count;
         }
-        
+        for (int i = 1; i <= y; i++)
+        {
+            b = i * K;
+            if (b == y)
+                ++count;
+        }
+        if (count == 2)
+            printf("Yes\n");
+        else
+            printf("No\n");
     }
     return 0;
 }
