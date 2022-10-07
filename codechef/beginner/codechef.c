441769ace70f53164d71d40557bdb6dbc98f725f
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-10-07 23:26:17 +0530

    Add files via upload

---
+++ codechef/beginner/codechef.c
@@ -0,0 +1,10 @@
+#include "codechef.h"
+
+int testcases()
+{
+    u5 T;
+    // int T;
+    printf("Enter the number of test cases: \n");
+    scanf("%d",&T);
+    return T;
+}
---
+++ codechef/beginner/codechef.h
@@ -0,0 +1,20 @@
+#ifndef __codechef_h_
+#define __codechef_h_
+
+typedef char u1;										// typedef bool u1 (for Borland, needs c++), hence used char here
+typedef float f32;
+typedef int u5;
+
+// typedef signed char s8;
+// typedef unsigned char u8;
+// typedef unsigned short int u16;
+// typedef short int s16;
+// typedef long signed int s32;
+// typedef long unsigned int u32;
+// typedef long signed int s64;			// long long not supported in Borland, hence used long
+// typedef long unsigned int u64;		// long long not supported in Borland, hence used long
+// typedef double f64;
+
+int testcases();
+
+#endif
