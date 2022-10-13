289321db74644da8c8bab17b31ac98613aac1a53
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-10-13 23:49:12 +0530

    Add files via upload

---
+++ codechef/intermediate/papercutting.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "codechef.c"
+
+#define papercutting
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[2];
+        inputarray(ipary,2);
+                
+    }
+    return 0;
+}
