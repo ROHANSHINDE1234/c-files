7759d2bed12dc1b597811a3c2cf820456e75bd91
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-10-17 21:29:27 +0530

    Update papercutting.c

--- codechef/intermediate/papercutting.c
+++ codechef/intermediate/papercutting.c
@@ -9,6 +9,19 @@ int main()
     {
         u5 ipary[2];
         inputarray(ipary,2);
+        for (u5 i = 1; i <= ipary[1]; i++)
+        {
+            ipary[0] = ipary[0] *i;
+            if (ipary[0]<=ipary[1])
+            {
+                j++;
+            }
+            else{
+                break;
+            }
+                        
+        }
+        printf("%d\n",j*j);
                 
     }
     return 0;
