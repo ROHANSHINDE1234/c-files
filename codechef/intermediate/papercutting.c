e85aed9ae48587137e5238d4c1adb54ce1e6500d
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-10-18 19:50:05 +0530

    Update papercutting.c

--- codechef/intermediate/papercutting.c
+++ codechef/intermediate/papercutting.c
@@ -7,21 +7,12 @@ int main()
     u5 T = testcases();
     while (T--)
     {
-        u5 ipary[2];
-        inputarray(ipary,2);
-        for (u5 i = 1; i <= ipary[1]; i++)
-        {
-            ipary[0] = ipary[0] *i;
-            if (ipary[0]<=ipary[1])
-            {
-                j++;
-            }
-            else{
-                break;
-            }
-                        
-        }
-        printf("%d\n",j*j);
+        u5 ipary[2], j = 0;
+        inputarray(ipary, 2);
+        printingarray(ipary, 2);
+        j = ipary[0] / ipary[1];
+        j = j * j;
+        printf("%d\n", j);
                 
     }
     return 0;
