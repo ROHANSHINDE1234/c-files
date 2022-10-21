73e3abdab602488406415c3ed16ce3377870a693
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-10-21 22:34:01 +0530

    Update bombthebase.c

--- codechef/intermediate/bombthebase.c
+++ codechef/intermediate/bombthebase.c
@@ -6,8 +6,18 @@ int main()
     u5 T = testcases();
     while (T--)
     {
-        u5 ipary[2];
-        inputarray(ipary,2);
+        u5 ipary[2], count = 0;
+        inputarray(ipary, 2);
+        u5 ipary1[ipary[0]];
+        inputarray(ipary1, ipary[0]);
+        for (u5 i = 0; i < ipary[0]; i++)
+        {
+            if (ipary1[i] < ipary[1])
+            {
+                count = i + 1;
+            }
+        }
+        printf("%d\n", count);
     }
     return 0;
 }
