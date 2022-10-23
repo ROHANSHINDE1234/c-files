e77a77b0601e56b0e1c3d8b004b59f9b12dde4e6
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-10-23 13:29:31 +0530

    Update concecutivecharater.c

--- codechef/intermediate/concecutivecharater.c
+++ codechef/intermediate/concecutivecharater.c
@@ -6,13 +6,20 @@ int main()
     u5 T = testcases();
     while (T--)
     {
-        u5 ipary[1], a;
+        u5 ipary[1], change=0;
         inputarray(ipary, 1);
-        printf("%d\n", ipary[0]);
-        a = ipary[0];
-        u1 Str[2];
-        gets(Str);
-        printf("%s\n", Str);
+        u1 S[ipary[0]];
+        printf("Enter your string of length %d\n", ipary[0]);
+        scanf("%s", S);
+        for (u5 i = 0; i < ipary[0]; i++)
+        {
+            u5 j = i + 1;
+            if (S[i] == S[j])
+            {
+                ++change;
+            }
+        }
+        printf("%d\n",change);
     }
     return 0;
 }
