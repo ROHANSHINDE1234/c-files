51d7eb509ac93da7294f2bc570114f856032f458
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-10-24 13:18:39 +0530

    Add files via upload

---
+++ codechef/intermediate/joiningdate.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "codechef.c"
+
+#define joiningdate
+
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[2];
+        inputarray(ipary, 2);
+        u5 ipary1[ipary[0]];
+        
+    }
+    return 0;
+}
