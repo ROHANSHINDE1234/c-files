1fbf73cb84eba642b88f434ad43f5cf74241aa09
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-11-07 22:21:26 +0530

    Add files via upload

---
+++ codechef/intermediate/puppyandsums.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "codechef.h"
+
+#define puppyandsums
+
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[2];
+        inputarray(ipary,2);
+        
+    }
+    return 0;
+}
