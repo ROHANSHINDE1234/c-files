a1ef4712a7abcb0a8f3b55aef39d359874c9ad13
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-11-08 22:04:14 +0530

    Update puppyandsums.c

--- codechef/intermediate/puppyandsums.c
+++ codechef/intermediate/puppyandsums.c
@@ -10,7 +10,11 @@ int main()
     {
         u5 ipary[2];
         inputarray(ipary,2);
-        
+        for (u5 i = 0; i < ipary[0]; i++)
+        {
+            ipary[1] = sumofnnumbers(ipary[1]);
+        }
+        printf("%d\n", ipary[1]);
     }
     return 0;
 }
