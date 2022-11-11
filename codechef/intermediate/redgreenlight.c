ea00f71ad1afc43bc203bfb13041b6f08787c942
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-11-11 21:04:16 +0530

    Add files via upload

---
+++ codechef/intermediate/redgreenlight.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "codechef.h"
+
+#define redgreenlight
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[2];
+        inputarray(ipary, 2);
+        u5 ipary1[ipary[0]];
+        inputarray(ipary1, ipary[0]);
+        }
+    return 0;
+}
