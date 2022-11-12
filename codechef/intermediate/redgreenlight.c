d971e51e4006b6ce58f648600914c58f27ae0422
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-11-12 20:01:15 +0530

    completed the code

--- codechef/intermediate/redgreenlight.c
+++ codechef/intermediate/redgreenlight.c
@@ -7,10 +7,18 @@ int main()
     u5 T = testcases();
     while (T--)
     {
-        u5 ipary[2];
+        u5 ipary[2], count = 0;
         inputarray(ipary, 2);
         u5 ipary1[ipary[0]];
         inputarray(ipary1, ipary[0]);
+        for (u5 i = 0; i < ipary[0]; i++)
+        {
+            if (ipary1[i] > ipary[1])
+            {
+                ++count;
+            }
+        }
+        printf("%d\n", count);
         }
     return 0;
 }
