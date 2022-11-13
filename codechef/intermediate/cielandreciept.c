495aa4dbd2e36a09af6bc2ba0fc66f16b5dcfb95
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-11-13 21:17:49 +0530

    Add files via upload

---
+++ codechef/intermediate/cielandreciept.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "codechef.h"
+
+#define cielandreciept
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[1];
+        inputarray(ipary,1);
+        
+    }
+    return 0;
+}
