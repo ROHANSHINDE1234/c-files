e63ab80212d02cf52c18fbb5d61981404d7a7b3b
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-11-22 20:50:23 +0530

    Add files via upload

---
+++ codechef/intermediate/waterconsumption.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "codechef.h"
+
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[1];
+        inputarray(ipary, 1);
+        if (ipary[0] >= 2000)
+        {
+            printf("Yes\n");
+        }
+        else
+        {
+            printf("No\n");
+        }
+    }
+    return 0;
+}
