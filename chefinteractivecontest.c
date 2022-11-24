e5e991e42005430b3694579c5d42379f2f0f5f00
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-11-24 22:10:28 +0530

    Add files via upload

---
+++ chefinteractivecontest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "codechef.h"
+
+int main()
+{
+    u5 T = testcases();
+    u5 A = testcases();
+    while (T--)
+    {
+        u5 ipary[T];
+        inputarray(ipary, T);
+    }
+    return 0;
+}
