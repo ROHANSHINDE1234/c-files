ee5adcab7b2fafcd3afc188438425aa254dab5be
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-11-30 21:49:55 +0530

    Add files via upload

---
+++ codechef/intermediate/fruitmarket.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "codechef.h"
+
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[4];
+        inputarray(ipary, 4);
+    }
+    return 0;
+}
