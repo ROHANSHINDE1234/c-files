a322ba1d326bf9700035c7da9017be55425eb692
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-12-14 22:29:05 +0530

    Add files via upload

---
+++ codechef/intermediate/makeap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "codechef.h"
+
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[2];
+        inputarray(ipary,2);
+    }
+    return 0;
+}
