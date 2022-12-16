7e794f1f311ad1da1ba7ba1485942561ff54e566
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-12-16 21:32:09 +0530

    Add files via upload

---
+++ codechef/intermediate/marathon.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "codechef.h"
+
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[5];
+        inputarray(ipary,5);
+    }
+    return 0;
+}
