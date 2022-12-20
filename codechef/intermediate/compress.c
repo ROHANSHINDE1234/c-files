d577ce900d3560f3665cdf87863db6f5769fdf74
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2022-12-20 23:02:00 +0530

    Add files via upload

---
+++ codechef/intermediate/compress.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "codechef.h"
+
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[1];
+        inputarray(ipary, 1);
+    }
+    return 0;
+}
