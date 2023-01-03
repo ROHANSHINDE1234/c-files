cbac8a6b24694a8fede87b18ca6ac3c9b60871c5
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-01-03 21:34:43 +0530

    Add files via upload

---
+++ codechef/intermediate/600 to 700/watching2x.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "codechef.h"
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[2];
+        inputarray(ipary, 2);
+        ipary[0] = ipary[0] - ipary[1];
+        ipary[1] = ipary[1] / 2;
+        printf("%d\n", ipary[0] + ipary[1]);
+    }
+    return 0;
+}
