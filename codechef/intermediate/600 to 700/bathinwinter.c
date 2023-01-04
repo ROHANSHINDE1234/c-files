93d50270d508a89446f8ae2a182476065f5628e5
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-01-04 19:23:51 +0530

    Add files via upload

---
+++ codechef/intermediate/600 to 700/bathinwinter.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "codechef.h"
+
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[2];
+        inputarray(ipary, 2);
+        ipary[1] = ipary[1] * 2;
+        ipary[0] = ipary[0] / ipary[1];
+        printf("%d\n", ipary[0]);
+    }
+    return 0;
+}
