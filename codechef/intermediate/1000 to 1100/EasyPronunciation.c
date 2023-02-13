5daddea988210573cd6109849c8a23f81af90a07
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-02-13 21:13:45 +0530

    Add files via upload

---
+++ codechef/intermediate/1000 to 1100/EasyPronunciation.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "codechef.h"
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[1];
+        inputarray(ipary,1);
+        u1 ipchar[ipary[0]];
+        for (u5 i = 0; i < ipary[0]; i++)
+        {
+            printf("%s\n",&ipchar[i]);
+        }
+        
+    }
+    return 0;
+}
