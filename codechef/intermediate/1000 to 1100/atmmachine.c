0fb120d25dbebf68606fa54d08766a3980311472
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-02-16 22:39:07 +0530

    Add files via upload

---
+++ codechef/intermediate/1000 to 1100/atmmachine.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "codechef.h"
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[2];
+        inputarray(ipary,2);
+        u5 ipary1[ipary[0]];
+        inputarray(ipary1,ipary[0]);
+        
+    }
+    return 0;
+}
