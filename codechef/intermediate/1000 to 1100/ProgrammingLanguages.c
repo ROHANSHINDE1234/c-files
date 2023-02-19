62b0343604e83108ddaccb0c18e37caafdf305ac
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-02-19 23:15:10 +0530

    Add files via upload

---
+++ codechef/intermediate/1000 to 1100/ProgrammingLanguages.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "codechef.h"
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[6];
+        inputarray(ipary, 6);
+    }
+    return 0;
+}
+/*
+Just know that starting two numbers needs to be compared with remaining two digits.
+
+*/
