a13b95501008d180025b9e7e68469b70ec3021b5
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-02-20 20:33:35 +0530

    Update ProgrammingLanguages.c

--- codechef/intermediate/1000 to 1100/ProgrammingLanguages.c
+++ codechef/intermediate/1000 to 1100/ProgrammingLanguages.c
@@ -7,10 +7,36 @@ int main()
     {
         u5 ipary[6];
         inputarray(ipary, 6);
+        if ((ipary[0] == ipary[2]) || (ipary[0] == ipary[3]))
+        {
+            if ((ipary[1] == ipary[2]) || (ipary[1] == ipary[3]))
+            {
+                printf("1\n");
+            }
+            else
+            {
+                printf("0\n");
+            }
+        }
+        else if ((ipary[0] == ipary[4]) || (ipary[0] == ipary[5]))
+        {
+            if ((ipary[1] == ipary[4]) || (ipary[1] == ipary[5]))
+            {
+                printf("2\n");
+            }
+            else
+            {
+                printf("0\n");
+            }
+        }
+        else
+        {
+            printf("0\n");
+        }
     }
     return 0;
 }
 /*
 Just know that starting two numbers needs to be compared with remaining two digits.
-
+First 2 numbers are paired together which should be equal to the next pairs.
 */
