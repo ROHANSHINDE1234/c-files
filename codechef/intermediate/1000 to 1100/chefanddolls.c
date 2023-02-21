d0eb9a7676e206deb1316e9271351af7361cff8e
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-02-21 23:37:18 +0530

    Add files via upload

---
+++ codechef/intermediate/1000 to 1100/chefanddolls.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "codechef.h"
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[1];
+        inputarray(ipary, 1);
+        u5 ipary1[ipary[0]];
+        inputarray(ipary1, ipary[0]);
+    }
+}
