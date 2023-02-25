a6044c6bc67715d4dc54477ab0e1f317165197fd
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-02-25 23:59:10 +0530

    Update chefanddolls.c

--- codechef/intermediate/1000 to 1100/chefanddolls.c
+++ codechef/intermediate/1000 to 1100/chefanddolls.c
@@ -7,17 +7,29 @@ int main()
     {
         u5 ipary[1];
         inputarray(ipary, 1);
-        u5 ipary1[ipary[0]];
+        u5 ipary1[ipary[0]], temp[ipary[0]];
         inputarray(ipary1, ipary[0]);
         for (u5 i = 0; i < ipary[0]; i++)
         {
+            u5 count;
             for (u5 j = 0; j < ipary[0]; j++)
             {
                 if (ipary1[i] == ipary1[j])
                 {
+                    count = count + 1;
                 }
             }
+            temp[i] = count;
         }
+        u5 num;
+        for (u5 i = 0; i < ipary[0]; i++)
+        {
+            if (temp[i] % 2 != 0)
+            {
+                num = i;
+            }
+        }
+        printf("%d\n", ipary[num]);
     }
 }
 /*
