37102883cd1667399e256a33616e54b1352b1439
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-02-26 17:20:39 +0530

    Add files via upload

---
+++ codechef/intermediate/1000 to 1100/SlowSolution.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "codechef.h"
+
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[3];
+        inputarray(ipary, 3);
+        
+    }
+    return 0;
+}
