9b7e804a70866483c3715757068a812ea0554708
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-03-06 23:13:13 +0530

    Add files via upload

---
+++ codechef/intermediate/1000 to 1100/zerooneequal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "codechef.h"
+int main()
+{
+    u5 T = testcases();
+    while (T--)
+    {
+        u5 ipary[1];
+        inputarray(ipary,1);
+    }
+    return 0;
+}
