5897a2490a7f84b6fe84f8f8d3b441255e904577
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-03-09 22:28:11 +0530

    Add files via upload

---
+++ codechef/intermediate/1000 to 1100/AdjacentSumParity.c
@@ -0,0 +1,9 @@
+/*
+Question is crystal clear
+A is an given array and B is formed by adding A's that particular i value with i+1 % with 2
+we are givem B and need to decide wether A exist or not.
+1) First we will take user define input // 09/03/2023
+2) Got to know that if % 2 is going to happen then input will be either 0 or 1.
+3) Try different combinations of even and odd numbers to find out which 1 & 0 is not possible.
+4) Then code accordingly. // 09/03/2023
+*/
