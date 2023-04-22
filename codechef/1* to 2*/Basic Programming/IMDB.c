5c1fd5254e7f357313289894d2a01e52284d2fae
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-04-22 20:17:47 +0530

    Update IMDB.c

--- codechef/1* to 2*/Basic Programming/IMDB.c
+++ codechef/1* to 2*/Basic Programming/IMDB.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
-
-int main(void) {
-	// your code goes here
+typedef int u4;
+u4 inputarray(u4 *array, u4 n)
+{
+    for(u4 i;i< n;i++)
+    {
+        scanf("%d",&array[i]);
+    }
+}
+u4 inputmatrix(u4 *matrix, u4 m, u4 n)
+{
+    for(u4 i=0;i<m;i++)
+    {
+        for(u4 j =0;j<n;j++)
+        {
+            scanf("%d",&matrix);
+        }
+    }
+}
+u4 testcases()
+{
+    u4 t;
+    scanf("%d",&t);
+    return t;
+}
+u4 main(void) {
+	u4 T;
+	T = testcases();
+	while(T--)
+	{
+	    u4 ipary[2];
+	    inputarray(ipary,2);
+	    u4 ipary1;
+	    inputmatrix(ipary1,ipary[0],2);
+	}
 	return 0;
 }
-
