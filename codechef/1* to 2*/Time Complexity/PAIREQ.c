7312e4cfccb6d92290bb52fa636d788669a710c6
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-05-03 22:16:07 +0530

    Add files via upload

---
+++ codechef/1* to 2*/Time Complexity/PAIREQ.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;  
+        scanf("%d",&n);
+        int a[n];
+        for(int i = 0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        
+    }
+	// your code goes here
+	return 0;
+}
+
