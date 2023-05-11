00340ee8f2a55408ad768643db5e830d6192be56
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-05-11 22:56:36 +0530

    Add files via upload

---
+++ codechef/intermediate/300 to 400/TAXSAVING.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b;
+        scanf("%d %d",&a,&b);
+        printf("%d\n",a-b);
+    }
+	// your code goes here
+	return 0;
+}
+
