1a3813f32af413cd7a02ffa09f70e1fe00b922b2
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-05-19 19:38:58 +0530

    Add files via upload

---
+++ codechef/intermediate/300 to 400/MY1STCONTEST.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int n,a,b;
+    scanf("%d %d %d",&n,&a,&b);
+    printf("%d\n",n-a);
+    printf("%d\n",n-a-b);
+	// your code goes here
+	return 0;
+}
+
