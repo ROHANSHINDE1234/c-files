c319c7c35617d41ba9d9e91539230980f91f5bce
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-05-24 20:05:38 +0530

    Add files via upload

---
+++ codechef/intermediate/300 to 400/CBSPEED.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+    int x,y;
+    scanf("%d %d",&x,&y);
+    if(y>x)
+    {
+        printf("Yes\n");
+    }
+    else
+    {
+        printf("No\n");
+    }
+	// your code goes here
+	return 0;
+}
+
