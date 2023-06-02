9ae199c64d973ef8c6aae8a8310caacd0b8df20d
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-06-02 22:13:44 +0530

    Update PAIREQ.c

--- codechef/1* to 2*/Time Complexity/PAIREQ.c
+++ codechef/1* to 2*/Time Complexity/PAIREQ.c
@@ -5,10 +5,10 @@ int main(void) {
     scanf("%d",&t);
     while(t--)
     {
-        int n;  
+        int n;
         scanf("%d",&n);
         int a[n];
-        for(int i = 0;i<n;i++)
+        for(i=0;i<n;i++)
         {
             scanf("%d",&a[i]);
         }
