bda2ed6ead35652ef00dfeb78a280c8229a404cb
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-06-05 22:30:16 +0530

    Add files via upload

---
+++ codechef/intermediate/300 to 400/KNGTOR.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,m;
+        scanf("%d %d",&n,&m);
+        printf("%d\n",(n*5)+(m*7));
+    }
+	// your code goes here
+	return 0;
+}
+
