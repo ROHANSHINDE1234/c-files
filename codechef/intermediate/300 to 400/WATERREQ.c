92c2ce40d27f51e43f0296f78e1b924eaaae4dc6
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-06-07 23:03:49 +0530

    Add files via upload

---
+++ codechef/intermediate/300 to 400/WATERREQ.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a;
+        scanf("%d",&a);
+        printf("%d\n",a*2);
+    }
+	// your code goes here
+	return 0;
+}
+
