e8c1ff9442b8a9c2b2f47e6d5ef3508c6d89c76c
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-06-08 21:06:13 +0530

    Add files via upload

---
+++ codechef/intermediate/300 to 400/CNTWRD.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,m;
+        scanf("%d %d",&n,&m);
+        printf("%d\n",n*m);
+    }
+	// your code goes here
+	return 0;
+}
+
