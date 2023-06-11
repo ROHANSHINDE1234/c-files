119f8a3b84f16234509fe678c5656cea5287eca6
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-06-11 22:29:08 +0530

    Update PAIREQ.c

--- codechef/1* to 2*/Time Complexity/PAIREQ.c
+++ codechef/1* to 2*/Time Complexity/PAIREQ.c
@@ -2,19 +2,35 @@
 
 int main(void) {
     int t;
-    scanf("%d",&t);
+    scanf("%d",&t); // Takes total number of test cases to be performed
     while(t--)
     {
         int n;
-        scanf("%d",&n);
-        int a[n];
+        scanf("%d",&n); // Takes the size of individual arrays
+        int a[n];  // Declares the array
         for(i=0;i<n;i++)
         {
-            scanf("%d",&a[i]);
+            scanf("%d",&a[i]);  // Takes the values of individual elements of array
         }
 	int max = 0;//small change
     }
 	// your code goes here
 	return 0;
 }
+/*
+What I understood 
+First take input for number of test cases put it in while loop 
+Took total number of elements we will have in that array
+then systematically took all the elements of that array
+Then I need to make all the elements of the array equal 
+for that i need to select what all number of the array are repeted the most
+then our final answer will be total length of array minus number of times 
+most repeted number.
 
+What I didn't understood
+How will i get how many times that a particular number is repeated.
+
+Solution
+Try to compare single number with every digit
+
+*/
