3296cd23bcd567f1c2f3663272316144701c6d03
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-06-17 23:28:50 +0530

    Add files via upload

---
+++ codechef/1* to 2*/Time Complexity/HOLIDAYS.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	return 0;
+}
+/*
+What I understood
+see after chef takes different number of testcases
+He takes number of days and total money to be collected
+In next line he take total number of money on individual day which can be
+earned.
+we need to find least number of working days required todo job to maximize 
+the holiday.
+*/
