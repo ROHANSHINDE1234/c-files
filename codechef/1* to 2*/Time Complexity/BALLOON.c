49b4078ec62b0c2629b87ba9ee0b8385cbcec677
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-06-20 22:06:44 +0530

    Add files via upload

---
+++ codechef/1* to 2*/Time Complexity/BALLOON.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	return 0;
+}
+/*
+What I understood
+We need to find least number of problems we need to solve to get
+1 to 7 for example if in the first 9 digits we get 1 to 7 then we can stop
+there and leave the rest.
+
+*/
