11331620143052b723c03afa258fa087ae419d14
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2023-07-02 22:55:12 +0530

    Update BALLOON.c

--- codechef/1* to 2*/Time Complexity/BALLOON.c
+++ codechef/1* to 2*/Time Complexity/BALLOON.c
@@ -9,5 +9,5 @@ What I understood
 We need to find least number of problems we need to solve to get
 1 to 7 for example if in the first 9 digits we get 1 to 7 then we can stop
 there and leave the rest.
-
+Try to put in for loop and increment a variable.
 */
