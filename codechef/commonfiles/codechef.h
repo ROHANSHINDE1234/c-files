31736046b1a66459ab7e0b1bc3161dd79ff41bd3
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2025-07-02 19:00:33 +0530

    Update codechef.h

--- codechef/commonfiles/codechef.h
+++ codechef/commonfiles/codechef.h
@@ -3,7 +3,7 @@
 
 typedef char u1;										// typedef bool u1 (for Borland, needs c++), hence used char here
 typedef float f32;
-typedef int u5;
+typedef int u32;
 typedef long long int l1;
 
 // typedef signed char s8;
@@ -16,13 +16,13 @@ typedef long long int l1;
 // typedef long unsigned int u64;		// long long not supported in Borland, hence used long
 // typedef double f64;
 
-u5 testcases();
-u6 printingarray(u5 *array,u5 n);
-u5 reversingarray(u5 *array, u5 n);
-u5 intconverttoarray(u5 *array, u5 count, u5 n);
-u5 countdigits(u5 a, u5 count);
-u5 inputarray(u5 *array,u5 n);
-u5 inputmatrix(u5 *matrix,u5 n,u5 m);
-u5 sumofnnumbers(u5 a);
+u32 testcases();
+u32 printingarray(u5 *array,u5 n);
+u32 reversingarray(u5 *array, u5 n);
+u32 intconverttoarray(u5 *array, u5 count, u5 n);
+u32 countdigits(u5 a, u5 count);
+u32 inputarray(u5 *array,u5 n);
+u32 inputmatrix(u5 *matrix,u5 n,u5 m);
+u32 sumofnnumbers(u5 a);
 
 #endif
