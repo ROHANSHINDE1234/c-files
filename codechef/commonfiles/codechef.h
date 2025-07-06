05f33de790f8c71e6c44fd2a16451300a97de419
ROHANSHINDE1234 <53709931+ROHANSHINDE1234@users.noreply.github.com>
2025-07-06 06:31:35 +0530

    Update codechef.h

--- codechef/commonfiles/codechef.h
+++ codechef/commonfiles/codechef.h
@@ -25,5 +25,6 @@ u32 inputarray(u32 *array,u32 n);
 u32 inputmatrix(u32 *matrix,u32 n,u32 m);
 u32 sumofnnumbers(u32 a);
 u32 arraysum(u32 *array, u32 n);
+bool checkrepeat(u32 *array,u32 n);
 
 #endif
